93c9405895a343aecb7cf2ad5ed12be6a823355a
ankuraga <ankuraga@gmail.com>
2015-03-03 21:39:20 -0800

    First Commit: Added NoteTaker App

---
+++ NoteTaker/app/src/main/java/android/ankur/com/notetaker/NoteEditorActivity.java
@@ -0,0 +1,61 @@
+package android.ankur.com.notetaker;
+
+import android.ankur.com.notetaker.data.NoteItem;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+/**
+ * Created by Ankur on 3/2/2015.
+ */
+public class NoteEditorActivity extends Activity {
+
+    private NoteItem note;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = this.getIntent();
+        note = new NoteItem();
+        note.setKey(intent.getStringExtra("key"));
+        note.setText(intent.getStringExtra("text"));
+
+        EditText editor = (EditText) findViewById(R.id.noteText);
+        editor.setText(note.getText());
+        editor.setSelection(note.getText().length());
+    }
+
+    private void saveAndFinish(){
+        EditText editor = (EditText) findViewById(R.id.noteText);
+        String noteText = editor.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("key", note.getKey());
+        intent.putExtra("text", noteText);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home){
+            saveAndFinish();
+        }
+        //This event has been handled (coz of save and finish...don't do anything else
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        saveAndFinish();
+    }
+}
+
---
+++ NoteTaker/app/src/main/java/android/ankur/com/notetaker/data/NotesDataSource.java
@@ -0,0 +1,59 @@
+package android.ankur.com.notetaker.data;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Created by Ankur on 3/2/2015.
+ */
+public class NotesDataSource {
+
+    private static final String PREFKEY = "notes";
+    private SharedPreferences notePrefs;
+
+    public NotesDataSource(Context context){
+        notePrefs = context.getSharedPreferences(PREFKEY, Context.MODE_PRIVATE);
+    }
+
+    public List<NoteItem> findAll(){
+
+        Map<String, ?> notesMap = notePrefs.getAll();
+        SortedSet<String> keys = new TreeSet<String>(notesMap.keySet());
+
+        List<NoteItem> noteList = new ArrayList<NoteItem>();
+        for (String key : keys)
+        {
+            NoteItem note = new NoteItem();
+            note.setKey(key);
+            note.setText((String) notesMap.get(key));
+            noteList.add(note);
+        }
+        return noteList;
+    }
+
+    public boolean update(NoteItem note){
+        SharedPreferences.Editor editor = notePrefs.edit();
+        editor.putString(note.getKey(), note.getText());
+        editor.commit();
+
+        return true;
+    }
+
+    public boolean remove(NoteItem note){
+
+        if (notePrefs.contains(note.getKey())) {
+            //Delete only if there is something to delete in the shared prefs
+            SharedPreferences.Editor editor = notePrefs.edit();
+            editor.remove(note.getKey());
+            editor.commit();
+        }
+        return true;
+    }
+}
