439a2521934e7a6ca2302e89f5c44064fd4b7ef2
ankuraga <ankuraga@gmail.com>
2015-03-04 23:46:22 -0800

    Code now reads and writes SharedPreferences

--- DataStorage_Preferences/app/src/main/java/android/ankur/com/datastorage_preferences/MainActivity.java
+++ DataStorage_Preferences/app/src/main/java/android/ankur/com/datastorage_preferences/MainActivity.java
@@ -1,17 +1,29 @@
 package android.ankur.com.datastorage_preferences;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = "DS_PREF";
+    public static final String USERNAME = "username";
+    public static final String DETAIL = "detail";
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prefs = getPreferences(MODE_PRIVATE);
+        getPrefs(findViewById(android.R.id.content));
     }
 
 
@@ -35,5 +47,23 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getPrefs(View v){
+        Log.i(TAG, "Getting prefs.");
+        EditText username = (EditText) findViewById(R.id.edit_User);
+        EditText detail = (EditText) findViewById(R.id.edit_Detail);
+        username.setText(prefs.getString(USERNAME, "Not Found"));
+        detail.setText(prefs.getString(DETAIL, "Not Found"));
+    }
+
+    public void setPrefs(View v){
+        Log.i(TAG, "Setting prefs.");
+        SharedPreferences.Editor editor = prefs.edit();
+        EditText username = (EditText) findViewById(R.id.edit_User);
+        EditText detail = (EditText) findViewById(R.id.edit_Detail);
+        editor.putString(USERNAME, String.valueOf(username.getText()));
+        editor.putString(DETAIL, String.valueOf(detail.getText()));
+        editor.commit();
     }
 }
