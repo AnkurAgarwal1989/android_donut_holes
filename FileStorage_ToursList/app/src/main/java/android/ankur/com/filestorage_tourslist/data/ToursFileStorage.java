90481485d20ce69faa97940d04ed4e9420b3a5e5
ankuraga <ankuraga@gmail.com>
2015-03-27 22:53:38 -0700

    Added new class to handle file reading and writing via external storage

---
+++ FileStorage_ToursList/app/src/main/java/android/ankur/com/filestorage_tourslist/data/ToursFileStorage.java
@@ -0,0 +1,8 @@
+package android.ankur.com.filestorage_tourslist.data;
+
+/**
+ * Created by Ankur on 3/27/2015.
+ */
+//Class to handle reading and writing to external storage for Tours File
+public class ToursFileStorage {
+}
